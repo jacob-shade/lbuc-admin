acfe1be506b2faf62860dece9729bfe9272bd0de
Jacob Shade <shadejacob@gmail.com>
2024-11-22 14:30:17 -0500

    separate health check to its own controller

--- server/controllers/userController.go
+++ server/controllers/userController.go
@@ -2,6 +2,6 @@ package controllers
 
 import "github.com/gofiber/fiber/v3"
 
-func Welcome(c fiber.Ctx) error {
+func UserController(c fiber.Ctx) error {
 	return c.SendString("welcome to the api👋!")
 }
