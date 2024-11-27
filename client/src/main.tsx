4437390fb057bc8f2dd7581f120b85d2910f2ab3
Jacob Shade <shadejacob@gmail.com>
2024-11-26 20:54:05 -0500

    basic sidebar added

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <SidebarProvider>
+      <div className="flex min-h-screen">
+        <div className="flex-shrink-0">
+          <AppSidebar />
+        </div>
+        <div className="flex flex-1 flex-col">
+          <div className="flex items-center p-4">
+            <SidebarTrigger />
+          </div>
+          <main>
+            <App />
+          </main>
+        </div>
+      </div>
+    </SidebarProvider>
+  </StrictMode> 
 )
